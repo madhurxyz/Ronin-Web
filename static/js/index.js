fd1622d49c54c9b8ae341509e4b62f01e7c9f0e2
Madhur Malhotra <malhotra.madhur15@gmail.com>
2016-12-13 16:49:05 -0800

    added api functionality

---
+++ static/js/index.js
@@ -0,0 +1,33 @@
+function SearchManager() {
+  this.submitButton = $('#submit-button');
+  this.urlSearchField = $('#url');
+  this.displayText = $('#answer');
+  this.submitButton.click(this._handleClick.bind(this));
+}
+
+
+SearchManager.prototype._handleClick = function() {
+  var url = this.urlSearchField.val();
+  this.displayText.html("Loading");
+  $.ajax({
+    method: "GET",
+    url: "https://news-hound-api.herokuapp.com/evaluate",
+    data: {url: url, ai: true},
+    success: this._handleSuccess.bind(this)
+  });
+}
+SearchManager.prototype._handleSuccess = function(response) {
+  var score = response.score;
+  if (score<50) {
+    this.displayText.html("Looks Fake");
+    document.body.style.backgroundImage = "url('../static/img/malicious.jpg')"
+  } else if (score<80) {
+    this.displayText.html("Questionable");
+    document.body.style.backgroundImage = "url('../static/img/questionable.jpg')"
+  } else {
+    this.displayText.html("Looks Good");
+    document.body.style.backgroundImage = "url('../static/img/credible.jpg')"
+  }
+}
+
+$(function(){new SearchManager()});
